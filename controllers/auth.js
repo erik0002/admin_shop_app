856d1eb49d6131c38789b048adaf40fbda6eab2c
Yeliazar <erik.ruzhakov@mail.ru>
2023-02-13 00:38:24 +0300

    add_cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
 exports.getLogin = (req, res, next) => {
-
+            // const isLoggedIn = req
+            //     .get('Cookie')
+            //     .split(";")
+            //     .trim()
+            //     .split('=')[4];
             res.render('auth/login', {
                 path: '/login',
-                pageTitle: 'Login'
+                pageTitle: 'Login',
+                isAuthenticated: false
             })
+};
+
+
+exports.postLogin = (req, res, next) => {
+    req.session.isLoggedIn = true;
+    res.redirect('/');
 };
